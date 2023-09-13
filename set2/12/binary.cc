d40ac8b96a0c8f70e260fff519aebf270366414a
GideonWiersma <g.j.wiersma@student.rug.nl>
2023-09-13 19:50:03 +0200

    working program, comments are still bad

--- set2/12/binary.cc
+++ set2/12/binary.cc
@@ -1,35 +1,62 @@
 #include <iostream>
 #include <string>
 #include <math.h>
+#include <typeinfo>
 
 int main()
 {
     using namespace std;
-    int Input;                         //declare
-    cin >> Input;                      //read command line
+    int value;                         //declare
+    cin >> value;                      //read command line
 
-    int sizeOfInput = sizeof(Input)*4; //determine size of input
+    int sizeOfvalue = sizeof(value)*4; //determine size of value
     
-    
-    string bitString;                 //string containing binary
-                                      // representation
+    //This string will contain everything up untill '<binary value> ='
+    string Output1;
+    //This string will contain the rest of the output            
+    string Output2;  
 
-    if (Input < 0)
+    Output1 += to_string(value); 
+    Output1 += " = ";
+
+    // in the this system
+    // - add 0 if first bit is 0
+    // - substract -32768 if first bit is -1
+    if (value >= 0)
     {
-        bitString += "-";             //determine sign of input
+        Output1 += '0'; // Positive thus first bit zero and add nothing
     }
-    //first determine the power of the bit
-    for(int power = sizeOfInput -1, value = Input;
-            power != -1; --power)
+    else
     {
+        Output1 += '1'; // Negative thus first bit one and substract -32768
+        
+        // do this by printing '-' and act as if the value is actually
+        // increased by 32768
+        Output2 += '-';
+        value += 32768; 
+    }
+
+    for(int power = sizeOfvalue -2;
+            power != -1; --power)
+    {   
         int bitValue = pow(2, power);
         int bit = value / pow(2, power);
-        int remainder = Input % bitValue;
+        int remainder = value % bitValue;
         
         value = remainder;
 
-        bitString += to_string(bit);
+        
+        if(bit)
+        {
+            Output2 += to_string(bitValue * bit) += " + ";
+        }
+
+        Output1 += to_string(bit);
     }
 
-    cout << bitString;
+    Output1 += " = ";
+    string::size_type on = Output2.length();
+    Output2.erase(on - 3, on);         //erase the last ' + '
+    cout << Output1 << Output2 << '\n';
+    
 }
