9ac9248ae170a8a9ba34dd2b93242a7ab6e135cb
GideonWiersma <g.j.wiersma@student.rug.nl>
2023-09-13 12:48:34 +0200

    syncing to online repository

---
+++ set1/4/oddEven.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+
+using namespace std;
+
+main()
+{
+    size_t value;
+    cin >> value;
+
+    cout << value%2
+}
---
+++ set1/6/oddEven.cc
@@ -0,0 +1,19 @@
+# include <iostream>
+# include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    size_t value;
+    // cin >> value;
+
+    value = ~0;
+
+    // cout << (value%2 == 0 ? "even" : "odd") << endl;
+    // cout << ( (value & 1) == 1 ? "odd" : "even") << endl;
+    cout << value << endl;
+
+
+    return 0;
+}
