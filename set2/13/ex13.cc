dd76f12f8ea983c9617123514180c651bcae43ff
GideonWiersma <g.j.wiersma@student.rug.nl>
2023-09-15 17:23:04 +0200

    unfinished

---
+++ set2/13/ex13.cc
@@ -0,0 +1,42 @@
+    #include <iostream>
+
+    int main()
+    {
+        using namespace std;
+        enum
+        {
+            BEGIN = 100,
+            END = 1000,
+            ADD = 32,
+            SUBS = 13
+        };
+
+        size_t size = END + ADD - (BEGIN - SUBS);
+
+        int squares[size];
+
+        size_t i = 0;
+        size_t square = 0;
+        while( square <= (END + ADD) );
+        {
+            square[]
+        }
+        
+
+        for(size_t n = 0, i = 0, square = 0;
+                 square <= END + ADD; ++n, ++i)
+        {
+            square = n*n;
+            squares[i] =  square;
+        }
+
+        for(size_t nr = BEGIN; nr != (END + 1); ++nr)
+            {
+                cout << squares[nr] << '\n';
+                size_t low = nr - SUBS;
+                size_t high = nr + ADD;
+
+                for(el in squares)
+            }
+
+    }
