7215bf6f8cb998159995554e68c84a6040e90bd1
GideonWiersma <g.j.wiersma@student.rug.nl>
2023-09-27 20:24:34 +0200

    fixed deletion of main.cc

--- set3/21/main.cc
+++ set3/21/main.cc
@@ -1,20 +1,19 @@
 #include "main.ih"
-
-int main(int argc, char **argv)
-{
-    if (argc == 1)
-    {
-        cout << "arg1: str to inspect, arg2: target, "
-                "optionally arg3: starting offset\n";
-        return 0;
-    }
-
-    size_t offset = strcasefind(argv[1], argv[2], 
-                                argc < 4 ? 0 : stoul(argv[3]));
-
-    cout << "strcasefind: `" << argv[2] << "' ";
-    if (offset == string::npos)
-        cout << "not found\n";
-    else
-        cout << "found at offset " << offset << '\n';
-}
+-
+-int main(int argc, char **argv)
+-{
+-    if (argc == 1)
+-    {
+-        cout << "arg1: str to inspect, arg2: target, "
+-                "optionally arg3: starting offset\n";
+-        return 0;
+-    }
+-
+-    size_t offset = strcasefind(argv[1], argv[2], 
+-                                argc < 4 ? 0 : stoul(argv[3]));
+-
+-    cout << "strcasefind: `" << argv[2] << "' ";
+-    if (offset == string::npos)
+-        cout << "not found\n";
+-    else
+-        cout << "found at offset " << offset << '\n';
